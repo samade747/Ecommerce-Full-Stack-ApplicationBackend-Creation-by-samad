186ad306c5849e876fcf32c1d332bce87b92e2db
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-08 19:44:18 +0500

    744

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
-import { Router } from "express";
+import { router } from "express";
+import User from "../models/User.js";
 
+router.post("/register", (req, res) => {
+    const newUser = new User({
+        
+    })
 
-
+    res.send("Login");
+})
 
 
 
