ac28fdad1f3629bd4386f5d860e774a0712f5aba
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-09 20:36:24 +0500

    836pm 9-6-24

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,18 @@
 import { router } from "express";
 import User from "../models/User.js";
 
-router.post("/register", (req, res) => {
-    const newUser = new User({
-        
-    })
 
-    res.send("Login");
+// REGISTER
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    // save user and respond
+    const savedUser = await newUser.save()
+    console.log(savedUser)
+    
 })
 
 
