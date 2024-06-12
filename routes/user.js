a0acf597b928dbacaba6438c719174550cf012b0
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-13 02:43:10 +0500

    242am

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,28 @@ import router from "express";
 import User from "../models/User.js";
 import CryptoJS from "crypto-js";
 import { verifyToken } from "../verifyToken.js";
+import { verifyTokenAndAuthorization } from "./verifyToken.js";
 
-router.put("/:id", verifyToken, async (req, res) => {
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.PASS_SEC
+            ).toString();
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id,
+                {
+                $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json(updatedUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
         next();
     } else {
         return res.status(403).json("You are not allowed!");
